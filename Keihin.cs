3cfa9c040a78c4cd83466a86580f84ad8bcdaabc
Tbichan <ryokatuo@yahoo.co.jp>
2016-04-26 23:37:50 +0900

    Added files via upload

---
+++ Keihin.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 景品を扱うクラスです。景品にアタッチしてください
+   変数raretypeはレア度を表す変数です。数値が高いほどレア度が高いです。 */
+
+public class Keihin : MonoBehaviour {
+
+    /////////////////////////////////////// フィールド ///////////////////////////////////////
+
+    // レア度、数値が高いほうがレア。
+    public int raretype;
+
+
+
+    //////////////////////////////////////// メソッド ////////////////////////////////////////
+
+    // レア度を取得する
+    public int getRareType()
+    {
+        return raretype;
+    }
+
+    // レア度を設定する
+    public void setRareType(int rare)
+    {
+        raretype = rare;
+    }
+
+    // レア度をランダムに設定する
+    public void setRareType()
+    {
+        System.Random r = new System.Random();
+
+
+
+        setRareType((new System.Random()).Next(4));
+    }
+}
---
+++ KeihinMgr.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 景品を検出し、結果を出力するクラスです。
+   景品を検出するオブジェクトにアタッチしてください。
+*/
+public class KeihinMgr : MonoBehaviour {
+
+
+    //////////////////////////////////////// メソッド ////////////////////////////////////////
+
+    // 初回の動作
+    void Start()
+    {
+
+    }
+
+    // 1フレームおきの動作
+    void Update()
+    {
+
+    }
+
+    // 接触したとき
+    void OnTriggerEnter(Collider c)
+    {
+        // 結果を表示する
+        keihinShow(c.gameObject);
+    }
+
+    // レア度を判定し、結果を表示する
+    void keihinShow(GameObject c)
+    {
+        // 景品以外は終了する
+        if (c.GetComponent<Keihin>() == null) return;
+
+        
+        switch (c.GetComponent<Keihin>().getRareType())
+        {
+
+            // ノーマル
+            case 0:
+                Debug.Log("ノーマルの景品です。");
+                break;
+
+            // レア
+            case 1:
+                Debug.Log("レアの景品です。");
+                break;
+
+            // スーパーレア
+            case 2:
+                Debug.Log("スーパーレアの景品です。");
+                break;
+
+            // ウルトラレア
+            default:
+                Debug.Log("ウルトラレアの景品です。");
+                break;
+        }
+    }
+
+}
