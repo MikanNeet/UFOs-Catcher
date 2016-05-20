c997fcab1c500a1e0a118e1a9951be881e1db6b0
yzcc <kaizoku_1113@yahoo.co.jp>
2016-05-20 18:13:10 +0900

    Update KeihinMgr.cs

--- KeihinMgr.cs
+++ KeihinMgr.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 /* 景品を検出し、結果を出力するクラスです。
    景品を検出するオブジェクトにアタッチしてください。
 */
 public class KeihinMgr : MonoBehaviour {
-
+    int rare_rand;
 
     //////////////////////////////////////// メソッド ////////////////////////////////////////
 
@@ -22,8 +22,9 @@ public class KeihinMgr : MonoBehaviour {
     }
 
     // 接触したとき
-    void OnTriggerEnter(Collider c)
+    void OnCollisionEnter(Collision c)
     {
+
         // 結果を表示する
         keihinShow(c.gameObject);
     }
@@ -34,30 +35,35 @@ public class KeihinMgr : MonoBehaviour {
         // 景品以外は終了する
         if (c.GetComponent<Keihin>() == null) return;
 
-        
         switch (c.GetComponent<Keihin>().getRareType())
         {
-
-            // ノーマル
-            case 0:
-                Debug.Log("ノーマルの景品です。");
-                break;
-
-            // レア
+            // ノーマル～レア
             case 1:
-                Debug.Log("レアの景品です。");
+                rare_rand = Random.Range(1, 71);
                 break;
 
-            // スーパーレア
+            // レア～スーパーレア
             case 2:
-                Debug.Log("スーパーレアの景品です。");
+                rare_rand = Random.Range(51,91);
                 break;
 
-            // ウルトラレア
+            // スーパーレア～ウルトラレア
             default:
-                Debug.Log("ウルトラレアの景品です。");
+                rare_rand = Random.Range(81, 101);
                 break;
         }
-    }
 
+        if (0 <= rare_rand && rare_rand <= 50) {
+            Debug.Log("ノーマル："+rare_rand);
+        }
+        else if (51 <= rare_rand && rare_rand <= 80) {
+            Debug.Log("レア："+rare_rand);
+        }
+        else if (81 <= rare_rand && rare_rand <= 95) {
+            Debug.Log("スーパーレア："+rare_rand);
+        }
+        else if (91 <= rare_rand && rare_rand <= 100) {
+            Debug.Log("ウルトラレア："+rare_rand);
+        }
+    }
 }
