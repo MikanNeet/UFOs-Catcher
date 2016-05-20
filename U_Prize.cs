ae0c2de07aa904f79618b15c4aa4f328b9830394
yzcc <kaizoku_1113@yahoo.co.jp>
2016-05-20 18:20:25 +0900

    Add files via upload

---
+++ U_Prize.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class U_Prize : MonoBehaviour {
+
+    GameObject oya;
+	// Use this for initialization
+	void Start () {
+        oya = GameObject.Find("GetArea");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider c) {
+        if (c.transform.tag == "get") {
+            this.transform.parent= oya.transform;
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.transform.tag == "get")
+        {
+            this.transform.parent = null;
+        }
+    }
+}
