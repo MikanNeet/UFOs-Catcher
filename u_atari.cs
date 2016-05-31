fb41488ea6886fa26a92f4177e9d2df4726118fc
yzcc <kaizoku_1113@yahoo.co.jp>
2016-05-31 13:46:37 +0900

    Add files via upload

---
+++ u_atari.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class u_atari : MonoBehaviour {
+
+    private int atari;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+    }
+	// Update is called once per frame
+	void Update () {
+
+	}
+ 
+    public int GetAtari() {
+        return atari;
+    }
+}
